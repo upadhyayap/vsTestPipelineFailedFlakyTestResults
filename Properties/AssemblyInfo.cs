21eb5bc13cda90a6704404085feb57a28c6341d2
Nikhil Verma <niver@microsoft.com>
2019-12-02 11:33:38 +0530

    rerunfiles

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FlakyTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FlakyTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("745b01fc-a778-4441-9a6c-524c29461c85")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FlakyTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private static int _value = 0;
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(_value %2 == 0);
+            _value++;
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.IsTrue(_value %2 == 0);
+            _value++;
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.IsTrue(_value %2 == 0);
+            _value++;
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Assert.IsTrue(_value %2 == 0);
+            _value++;
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            //Always pass
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            //Always pass
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+            Assert.IsTrue(false, "It will always fail");
+        }
+
+        [TestMethod]
+        public void TestMethod8()
+        {
+            Assert.IsTrue(false, "It will always fail");
+        }
+    }
+}
