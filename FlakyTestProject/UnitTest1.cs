557d3fa57130fb0907d15e0913b0387b4992ec2f
Nikhil Verma <niver@microsoft.com>
2019-12-02 12:32:18 +0530

    priority

--- FlakyTestProject/UnitTest1.cs
+++ FlakyTestProject/UnitTest1.cs
@@ -8,6 +8,7 @@ namespace FlakyTestProject
         private static int _value = 0;
 
         [TestMethod]
+        [Priority(0)]
         public void TestMethod1()
         {
             Assert.IsTrue(_value %2 == 0);
@@ -15,6 +16,7 @@ namespace FlakyTestProject
         }
 
         [TestMethod]
+        [Priority(0)]
         public void TestMethod2()
         {
             Assert.IsTrue(_value %2 == 0);
@@ -22,6 +24,7 @@ namespace FlakyTestProject
         }
 
         [TestMethod]
+        [Priority(0)]
         public void TestMethod3()
         {
             Assert.IsTrue(_value %2 == 0);
@@ -29,6 +32,7 @@ namespace FlakyTestProject
         }
 
         [TestMethod]
+        [Priority(0)]
         public void TestMethod4()
         {
             Assert.IsTrue(_value %2 == 0);
@@ -36,24 +40,28 @@ namespace FlakyTestProject
         }
 
         [TestMethod]
+        [Priority(0)]
         public void TestMethod5()
         {
             //Always pass
         }
 
         [TestMethod]
+        [Priority(0)]
         public void TestMethod6()
         {
             //Always pass
         }
 
         [TestMethod]
+        [Priority(0)]
         public void TestMethod7()
         {
             Assert.IsTrue(false, "It will always fail");
         }
 
         [TestMethod]
+        [Priority(0)]
         public void TestMethod8()
         {
             Assert.IsTrue(false, "It will always fail");
